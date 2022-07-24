10842e0bec1e570627836494940b238ccbefb6b5
yuanLeeMidori <midori3113133@gmail.com>
2022-07-24 17:44:45 -0400

    reverse.js passed

--- Recursion/Challenging/reverse.js
+++ Recursion/Challenging/reverse.js
@@ -7,30 +7,16 @@ function reverse(str) {
     let r_str = '';
     // base case
     // when the string is empty
+    if (str.length === 0) return '';
 
     // diff input
-    // slice the last ?
-    console.log(str.length);
+    // add and remove the last
+    r_str += str.substring(str.length - 1, str.length);
+    
+    return r_str + reverse(str.substring(0, str.length - 1));
 
-    r_str += str.substring(str.length-1, str.length);
-    str = str.substring(0, str.length-1);
-    console.log(r_str);
-    console.log(str);
-    console.log(str.length);
-
-    r_str += str.substring(str.length-1, str.length);
-    str = str.substring(0, str.length-1);
-    console.log(r_str);
-    console.log(str);
-    console.log(str.length);
-
-    r_str += str.substring(str.length-1, str.length);
-    str = str.substring(0, str.length-1);
-    console.log(r_str);
-    console.log(str);
-    console.log(str.length);
-
-    return;
 }
 
-console.log(reverse('dog'));
+console.log(reverse('abcdefghijklmno'));
+console.log(reverse('! abcdefghijkl mno'));
+console.log(reverse(''));
